66050a7edcf7d195357f49c7a2db8bad379b536c
pavellev <pavellev@inter.net.il>
2020-12-13 19:14:54 +0200

    Unit test optimisation

--- main.c
+++ main.c
@@ -9,27 +9,24 @@ int main (int argc, const char * argv[])
     bool rc_flash_full;
     bool rc_write_read;
     bool rc_read_all;
-    int rc = -1;  // initialise negative return code value
+    bool rc;
 
     logger_init();
 
     // below tests sequence order is important !
     rc_mode_validity = test_mode_validity();
     rc_flash_empty   = test_flash_empty();
-    rc_write_read    = test_write_read();
-    logger_init();
     rc_flash_full    = test_flash_full();
+    logger_init();
+    rc_write_read    = test_write_read();
     logger_init();
     rc_read_all      = test_read_all();
 
-    if (rc_mode_validity &&
-        rc_flash_empty &&
-        rc_flash_full &&
-        rc_write_read &&
-        rc_read_all)
-    {
-        return 0;
-    }
+    rc = rc_mode_validity &&
+         rc_flash_empty &&
+         rc_flash_full &&
+         rc_write_read &&
+         rc_read_all;
 
-    return rc;
+    return rc ? 0 : 1;
 }
